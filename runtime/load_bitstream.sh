12c84519f867da84b9037a80237379473a51bd53
DouglasWWolf <douglaswolf@yahoo.com>
2024-04-30 14:16:49 -0700

    No change history kept

--- runtime/load_bitstream.sh
+++ runtime/load_bitstream.sh
@@ -1,1 +1,1 @@
-sudo ./load_bitstream -hot_reset -config load_jtag.conf -part xcvu3p_0 -jtag_id $XUPPL4_JTAG_ID $PWD/xuppl4_packet_capture.bit
+load_bitstream -hot_reset -config load_jtag.conf -part xcvu3p_0 -jtag_id $XUPPL4_JTAG_ID $PWD/xuppl4_packet_capture.bit
